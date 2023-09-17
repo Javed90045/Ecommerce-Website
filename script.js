54e96f0b2e44506ef59d71ea6afcf53cdd32f934
javed <javedali90045@gmail.com>
2023-09-17 13:37:15 +0530

    We create hamburger function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Script for navigation bar
+// right side navbar toggle
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+if(bar){
+    bar.addEventListener('click',() =>{
+        nav.classList.add('active');
+    })
+}
+
+// right side navbar close button
+const close = document.getElementById('close');
+if(close){
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+}
